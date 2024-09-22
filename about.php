a38afa4ecc3aabac740ab125e847d69d74dc5d71
ISSE0116 <yukizaru.2587@icloud.com>
2024-09-22 20:57:22 +0900

    update
    プロフィールページ追加

--- about.php
+++ about.php
@@ -1,3 +1,5 @@
+
+
 <?php include 'db_connect.php'; ?>
 
 <!DOCTYPE html>
@@ -24,35 +26,53 @@
     <?php include 'navbar.php'; ?>
 
     <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-8">
-                <h2><strong style="font-size: 1em; color: #2c3e50;">Issei Kikuchi</strong></h2>
-
-                <h4>Affiliation</h4>
-                <p>graduate student</p>
-                <!--
-                <h4>Research Topic</h4>
-                <p>The Development of an Exercise Support System Using Spatial Audio</p>
-                -->
-                <h4>Hobbies</h4>
-                <ul class="indented-list">
-                    <li>cycling</li>
-                    <li>photography</li>
-                </ul>
-
-                <h4>Contact</h4>
-                <ul>
-                    <li>Email: <a href="mailto:yukizaru.2587@icloud.com">yukizaru.2587@icloud.com</a></li>
-                </ul>
-
-                <h4>SNS</h4>
-                <ul class="list-inline">
-                    <li class="list-inline-item"><a href="https://www.instagram.com/issei__masaoka" target="_blank"><i class="fab fa-instagram fa-2x"></i></a></li>
-                    <li class="list-inline-item"><a href="https://github.com/ISSE0116" target="_blank"><i class="fab fa-github fa-2x"></i></a></li>
-                </ul>
+        <div class="text-center">
+            <!-- プロフィール写真 -->
+            <div class="profile-photo mb-4">
+                <img src="./images/con.jpg" alt="Profile Photo" class="rounded-circle">
             </div>
+
+            <!-- 名前と所属 -->
+            <h2><strong style="font-size: 1.5em; color: #2c3e50;">Issei Kikuchi</strong></h2>
+            <h4>Graduate Student</h4>
+
+            <!-- 趣味 -->
+            <h4>Hobbies</h4>
+            <ul class="list-inline">
+                <li class="list-inline-item">Cycling</li>
+                <li class="list-inline-item">Photography</li>
+            </ul>
+
+            <!-- スキル -->
+            <h4>Skills(Language,Framework)</h4>
+            <ul class="list-inline">
+                <li class="list-inline-item">C</li>
+                <li class="list-inline-item">C++</li>
+                <li class="list-inline-item">C#</li>
+                <li class="list-inline-item">Python</li>
+                <li class="list-inline-item">JavaScript</li>
+                <li class="list-inline-item">HTML/CSS</li>
+                <li class="list-inline-item">AWS</li>
+                <li class="list-inline-item">Flask</li>
+                <li class="list-inline-item">React</li>
+                <li class="list-inline-item">AWS</li>
+                <li class="list-inline-item">Pytorch</li>
+            </ul>
+
+            <!-- 連絡先 -->
+            <h4>Contact</h4>
+            <ul class="list-inline">
+                <li class="list-inline-item">Email: <a href="mailto:yukizaru.2587@icloud.com">yukizaru.2587@icloud.com</a></li>
+            </ul>
+
+            <!-- SNS -->
+            <h4>SNS</h4>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="https://www.instagram.com/issei__masaoka" target="_blank"><i class="fab fa-instagram fa-2x"></i></a></li>
+                <li class="list-inline-item"><a href="https://github.com/ISSE0116" target="_blank"><i class="fab fa-github fa-2x"></i></a></li>
+            </ul>
         </div>
     </div>
+
 </body>
 </html>
-
